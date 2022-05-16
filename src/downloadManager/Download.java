7c2839c480f03a86dcc579d72ec962b2d71c464a
hajunKwon <gkwns5791@naver.com>
2022-05-16 13:23:47 +0900

    주석

--- src/downloadManager/Download.java
+++ src/downloadManager/Download.java
@@ -4,138 +4,136 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-// This class downloads a file from a URL.
+// URL에서부터 파일을 다운로드
 class Download extends Observable implements Runnable {
   // Max size of download buffer.
   private static final int MAX_BUFFER_SIZE = 1024;
 
-  // These are the status names.
+  // 상태 코드 이름
   public static final String STATUSES[] = {"Downloading",
     "Paused", "Complete", "Cancelled", "Error"};
 
-  // These are the status codes.
+  // 상태 코드
   public static final int DOWNLOADING = 0;
   public static final int PAUSED = 1;
   public static final int COMPLETE = 2;
   public static final int CANCELLED = 3;
   public static final int ERROR = 4;
 
-  private URL url; // download URL
-  private int size; // size of download in bytes
-  private int downloaded; // number of bytes downloaded
-  private int status; // current status of download
+  private URL url; // 다운로드할 URL
+  private int size; // 다운로드할 파일의 크기(바이트)
+  private int downloaded; // 다운로드한 크기(바이트)
+  private int status; // 다운로드의 현재 상태
 
-  // Constructor for Download.
+  // 다운로드의 생성자
   public Download(URL url) {
     this.url = url;
     size = -1;
     downloaded = 0;
     status = DOWNLOADING;
 
-    // Begin the download.
+    // 다운로드 시작
     download();
   }
 
-  // Get this download's URL.
+  // 다운로드할 URL을 얻음
   public String getUrl() {
     return url.toString();
   }
 
-  // Get this download's size.
+  // 다운로드할 파일의 크기를 얻음
   public int getSize() {
     return size;
   }
 
-  // Get this download's progress.
+  // 다운로드의 진행률을 얻음
   public float getProgress() {
     return ((float) downloaded / size) * 100;
   }
 
-  // Get this download's status.
+  // 다운로드의 상태를 얻음
   public int getStatus() {
     return status;
   }
 
-  // Pause this download.
+  // 다운로드 정지
   public void pause() {
     status = PAUSED;
     stateChanged();
   }
 
-  // Resume this download.
+  // 다운로드 재개
   public void resume() {
     status = DOWNLOADING;
     stateChanged();
     download();
   }
 
-  // Cancel this download.
+  // 다운로드 취소
   public void cancel() {
     status = CANCELLED;
     stateChanged();
   }
 
-  // Mark this download as having an error.
+  // 이 다운로드에 에러가 있응을 표시
   private void error() {
     status = ERROR;
     stateChanged();
   }
 
-  // Start or resume downloading.
+  // 다운로드를 시작하거나 재개
   private void download() {
     Thread thread = new Thread(this);
     thread.start();
   }
 
-  // Get file name portion of URL.
+  // URL에서 파일 이름 부분을 얻음
   private String getFileName(URL url) {
     String fileName = url.getFile();
     return fileName.substring(fileName.lastIndexOf('/') + 1);
   }
 
-  // Download file.
+  // 파일 다운로드
   public void run() {
     RandomAccessFile file = null;
     InputStream stream = null;
 
     try {
-      // Open connection to URL.
+      // URL 연결
       HttpURLConnection connection =
         (HttpURLConnection) url.openConnection();
 
-      // Specify what portion of file to download.
+      // 파일의 어느 부분 다운로드할 것인지 명세
       connection.setRequestProperty("Range",
         "bytes=" + downloaded + "-");
 
-      // Connect to server.
+      // 서버에 접속
       connection.connect();
 
-      // Make sure response code is in the 200 range.
+      // 응답 코드가 200번 대에 있는지 확인
       if (connection.getResponseCode() / 100 != 2) {
         error();
       }
 
-      // Check for valid content length.
+      // 유효한 contentlength를 검사
       int contentLength = connection.getContentLength();
       if (contentLength < 1) {
         error();
       }
 
-      /* Set the size for this download if it
-         hasn't been already set. */
+      // 아직 다운로드에 대한 크기가 설정되지 않았으면 설정
       if (size == -1) {
         size = contentLength;
         stateChanged();
       }
 
-      // Open file and seek to the end of it.
+      // 파일을 연 다음, 파일 포인터를 파일의 끝으로 이동
       file = new RandomAccessFile(getFileName(url), "rw");
       file.seek(downloaded);
 
       stream = connection.getInputStream();
       while (status == DOWNLOADING) {
-        /* Size buffer according to how much of the
-           file is left to download. */
+        // 다운로드할 부분이 얼마나 더 남아 있는지에 다라 버퍼의 크기를 조절
         byte buffer[];
         if (size - downloaded > MAX_BUFFER_SIZE) {
           buffer = new byte[MAX_BUFFER_SIZE];
@@ -143,19 +141,19 @@ class Download extends Observable implements Runnable {
           buffer = new byte[size - downloaded];
         }
 
-        // Read from server into buffer.
+        // 서버에서부터 버퍼로 읽어옴
         int read = stream.read(buffer);
         if (read == -1)
           break;
 
-        // Write buffer to file.
+        // 버퍼의 내용을 파일에 씀
         file.write(buffer, 0, read);
         downloaded += read;
         stateChanged();
       }
 
-      /* Change status to complete if this point was
-         reached because downloading has finished. */
+      // 이 지점에 도닳하면 다운로드가 끝났음을 의미
+      // 상태값을 완료로 바꿈
       if (status == DOWNLOADING) {
         status = COMPLETE;
         stateChanged();
@@ -163,14 +161,14 @@ class Download extends Observable implements Runnable {
     } catch (Exception e) {
       error();
     } finally {
-      // Close file.
+      // 파일 닫음
       if (file != null) {
         try {
           file.close();
         } catch (Exception e) {}
       }
 
-      // Close connection to server.
+      // 서버 연결을 닫음
       if (stream != null) {
         try {
           stream.close();
@@ -179,7 +177,7 @@ class Download extends Observable implements Runnable {
     }
   }
 
-  // Notify observers that this download's status has changed.
+  // 다운로드 상태가 변경되었음을 관찰자들에게 알림
   private void stateChanged() {
     setChanged();
     notifyObservers();
